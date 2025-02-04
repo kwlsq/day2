bfffef71051d6c0d6a882c54baaeb2874bd565e7
Vincentius <vincentiussss@gmail.com>
2025-02-04 15:48:09 +0700

    feat(validator): create a palindrome validator

--- src/main/java/org/vincent/ValidatorUtils.java
+++ src/main/java/org/vincent/ValidatorUtils.java
@@ -1,7 +1,18 @@
 package org.vincent;
 
+import java.util.Objects;
+
 public class ValidatorUtils {
     public static boolean evenValidators(int number){
         return number % 2 == 0;
     }
+
+    public static String palindromeValidators(String inputString){
+        if(Objects.equals(inputString.toLowerCase(), StringUtils.stringReverser(inputString).toLowerCase())){
+            return "palindrome";
+        } else {
+            return "not palindrome";
+        }
+    }
+
 }
