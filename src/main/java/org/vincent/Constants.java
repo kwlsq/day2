05fdbac124acfc828ad8fc11fcd76359e5869521
Vincentius <vincentiussss@gmail.com>
2025-02-04 14:57:04 +0700

    feat(constants): add separate class for handling all constants

---
+++ src/main/java/org/vincent/Constants.java
@@ -0,0 +1,6 @@
+package org.vincent;
+
+public class Constants {
+    static double TEMPERATURE = 1.5;
+    static double LENGTH = 200000;
+}
--- src/main/java/org/vincent/Main.java
+++ src/main/java/org/vincent/Main.java
@@ -3,15 +3,14 @@ package org.vincent;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static double temperature = 1.5;
-    static double length = 200000;
+
     public static void main(String[] args) {
-        System.out.println(temperature + " Fahrenheit = "
-                + ConverterUtils.fahrenheitToCelsiusConverter(temperature) + " Celsius"
+        System.out.println(Constants.TEMPERATURE + " Fahrenheit = "
+                + ConverterUtils.fahrenheitToCelsiusConverter(Constants.TEMPERATURE) + " Celsius"
         );
 
-        System.out.println(length + " cm = "
-                + ConverterUtils.centimeterToKilometerConverter(length) + " km"
+        System.out.println(Constants.LENGTH + " cm = "
+                + ConverterUtils.centimeterToKilometerConverter(Constants.LENGTH) + " km"
         );
 
     }
