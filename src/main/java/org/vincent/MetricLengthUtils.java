83529f78dc40de0bfba7e582f4198d1ffeec4c82
Vincentius <vincentiussss@gmail.com>
2025-02-04 14:47:21 +0700

    feat(length): create cm to km converter logic

--- src/main/java/org/vincent/MetricLengthUtils.java
+++ src/main/java/org/vincent/MetricLengthUtils.java
@@ -1,4 +1,11 @@
 package org.vincent;
 
 public class MetricLengthUtils {
+    public static double centimeterToKilometerConverter(double length){
+        if(length < 0) {
+            throw new IllegalArgumentException("Length must be positive numbers");
+        }
+
+        return length / 100000;
+    }
 }
