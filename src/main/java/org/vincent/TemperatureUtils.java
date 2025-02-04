4ae4a2b9db05763a3a8a090f1c6bf709165940de
Vincentius <vincentiussss@gmail.com>
2025-02-04 14:29:34 +0700

    feat(temperature): create Fahrenheit to Celsius converter

--- src/main/java/org/vincent/TemperatureUtils.java
+++ src/main/java/org/vincent/TemperatureUtils.java
@@ -1,4 +1,7 @@
 package org.vincent;
 
 public class TemperatureUtils {
+    public static float fahrenheitToCelsiusConverter(float temperature){
+        return ((temperature-32) * ((float) 5 /9));
+    }
 }
