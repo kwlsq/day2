b8d849edfb00e515efc50e1540b0cda1f7711ecb
Vincentius <vincentiussss@gmail.com>
2025-02-04 14:49:40 +0700

    feat(length): call metricLengthUtils in main

--- src/main/java/org/vincent/Main.java
+++ src/main/java/org/vincent/Main.java
@@ -4,10 +4,16 @@ package org.vincent;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static double temperature = 1.5;
+    static double length = 200000;
     public static void main(String[] args) {
         System.out.println(
                 temperature + " Fahrenheit = "
                 + TemperatureUtils.fahrenheitToCelsiusConverter(temperature) + " Celsius"
+        );
+
+        System.out.println(
+                length + " cm = "
+                        + MetricLengthUtils.centimeterToKilometerConverter(length) + " km"
         );
 
     }
