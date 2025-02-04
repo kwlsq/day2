9303afc95cd458cdf8349c6605c761805d8545e1
Vincentius <vincentiussss@gmail.com>
2025-02-04 14:38:05 +0700

    feat(temperature): call temperature utils and printout in main

--- src/main/java/org/vincent/Main.java
+++ src/main/java/org/vincent/Main.java
@@ -3,7 +3,12 @@ package org.vincent;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static double temperature = 1.5;
     public static void main(String[] args) {
+        System.out.println(
+                temperature + " Fahrenheit = "
+                + TemperatureUtils.fahrenheitToCelsiusConverter(temperature) + " Celsius"
+        );
 
     }
 }
